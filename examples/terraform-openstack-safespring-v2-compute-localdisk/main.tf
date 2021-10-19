9a00b6d2b4ab33e24eaf192f868c1246bdd0d5b4
Jarle Bjørgeengen <jarle@safespring.com>
2021-10-19 12:50:35 +0200

    CEM-16 Forgot to add outputs.tf. Use git-url for the second example too

--- examples/terraform-openstack-safespring-v2-compute-localdisk/main.tf
+++ examples/terraform-openstack-safespring-v2-compute-localdisk/main.tf
@@ -2,13 +2,14 @@
 # possible values and descriptions
 
 module my_sf_instances_tiny_a_set {
-  #source = "../../terraform-openstack-safespring-v2-compute-localdisk"
+  # source = "../../terraform-openstack-safespring-v2-compute-localdisk"
   source = "github.com/safespring-community/terraform-modules/terraform-openstack-safespring-v2-compute-localdisk"
   key_pair_name = "jarleb"
 }
 
 module my_sf_instances_another_set{
-  source = "../../terraform-openstack-safespring-v2-compute-localdisk"
+  # source = "../../terraform-openstack-safespring-v2-compute-localdisk"
+  source = "github.com/safespring-community/terraform-modules/terraform-openstack-safespring-v2-compute-localdisk"
   key_pair_name = "jarleb"
   instance_count = 2
   image = "centos-7"
