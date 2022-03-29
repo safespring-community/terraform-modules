a1cbacc2272f0e36daf8b7b9ee381f5c288cf282
Jarle Bjørgeengen <jarle@safespring.com>
2022-03-29 10:51:09 +0200

    Updated example

--- examples/v2-compute-instance/main.tf
+++ examples/v2-compute-instance/main.tf
@@ -1,16 +1,18 @@
 # See variables.tf for 
 # possible values and descriptions
 
+
 module my_sf_instance {
-   source = "github.com/safespring-community/terraform-modules/v2-compute-instance"
-   # name          = "pangalactic-gargleblaster"
-   # key_pair_name = "your-keypair"
-   # config_drive  = false
-   # disk_size     = 5                 # When using b2-flavors
-   # network       = "default"         # One of default, private, public
-   # role          = "general"         # Ends up as metadata. Can be for example be used as ansible host group with Ansible Terraform Inventory (ATI)
-   # image         = "ubuntu-20.04"
-   # flavor        = "l2.c2r4.100"     # Use openstack flavor list. Pick flavors starting with b2 or l2 
+   # source          = "github.com/safespring-community/terraform-modules/v2-compute-instance"
+   # name            = "hello-safespring-1.example.com"
+   # key_pair_name   = "your-keypair"
+   # config_drive    = false
+   # disk_size       = 5                 # When using b2-flavors
+   # network         = "default"         # One of default, private, public
+   # wg_ip           = ""                # Ends up as metadata. Can be used to assign wireguard address for us in Ansible.
+   # role            = "general"         # Ends up as metadata. Can be for example be used as ansible host group with Ansible Terraform Inventory (ATI)
+   # image           = "ubuntu-20.04"
+   # flavor          = "l2.c2r4.100"     # Use openstack flavor list. Pick flavors starting with b2 or l2 
    # security_groups = ["default"]
    # data_disks = {
    #   "db" = {
