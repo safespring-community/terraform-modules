a00ff72376dee006b6b684db43e70c4cba2dd288
Jarle Bjørgeengen <jarle@safespring.com>
2022-05-24 16:54:09 +0200

    CEM-125 WorkInProgress

---
+++ examples/v2-compute-instance-with-web-service/main.tf
@@ -0,0 +1,77 @@
+terraform {
+  required_version = ">= 0.14.0"
+  required_providers {
+    openstack = {
+      source  = "terraform-provider-openstack/openstack"
+    }
+  }
+}
+
+# Create a keypair from a public key.
+# An openstack keypair contains only the public key. Thus a misleading name for it.
+resource "openstack_compute_keypair_v2" "skp" {
+  name       = "hello-pubkey"
+  public_key = "${chomp(file("~/.ssh/id_rsa_jump.pub"))}"
+}
+
+module ingress {
+   source = "github.com/safespring-community/terraform-modules/v2-compute-security-group"
+   name = "ingress"
+   delete_default_rules = true
+   description = "For exposing web servers on port 443 (https) to the world"
+   rules = {
+     egress = {
+       direction   = "egress"
+       ethertype   = "IPv4"
+       cidr        = "0.0.0.0/0"
+     }
+     ssh = {
+       direction   = "ingress"
+       ip_protocol = "tcp"
+       to_port     = "22"
+       from_port   = "22"
+       ethertype   = "IPv4"
+       cidr        = "0.0.0.0/0"
+     }
+     http = {
+       direction   = "ingress"
+       ip_protocol = "tcp"
+       to_port     = "80"
+       from_port   = "80"
+       ethertype   = "IPv4"
+       cidr        = "0.0.0.0/0"
+     }
+  }
+}
+
+locals {
+  instances = {
+    "web1" = {
+      name    = "websrv1.example.com"
+      flavor  = "l2.c2r4.100"
+      os      = "ubuntu-20.04"
+      network = "public"
+      role    = "webserver"
+      sgs     = [ module.ingress.name ]
+    }
+    "web2" = {
+      name    = "websrv2.example.com"
+      flavor  = "l2.c2r4.100"
+      os      = "ubuntu-20.04"
+      network = "public"
+      role    = "webserver"
+      sgs     = [ module.ingress.name ]
+    }
+  }
+}
+
+module my_sf_instances {
+   for_each        = local.instances
+   source          = "github.com/safespring-community/terraform-modules/v2-compute-instance"
+   name            = each.value.name
+   image           = each.value.os
+   network         = each.value.network
+   security_groups = each.value.sgs
+   role            = each.value.role
+   key_pair_name   = openstack_compute_keypair_v2.skp.name
+}
