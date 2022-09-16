3631eb2f62f895d7b2ff2232fd2ebaf4a07affb7
Jarle Bjørgeengen <jarle@safespring.com>
2022-09-16 12:40:56 +0200

    CEM-187 Use offical release of gandi provider

--- v2-okd-cluster-gandi-dns/workers/versions.tf
+++ v2-okd-cluster-gandi-dns/workers/versions.tf
@@ -9,8 +9,8 @@ terraform {
       version = "~> 1.35"
     }
     gandi = {
-      version = "2.0.0-rc3" 
-      source   = "psychopenguin/gandi"
+      version = "~> 2.1.0"
+      source   = "go-gandi/gandi"
     }
   }
   required_version = ">= 0.13"
