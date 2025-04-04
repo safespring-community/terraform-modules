0f8e688fde8c0b26960de645c9ce8cb46780c2aa
stefan <stefan.negru@safespring.com>
2025-04-04 11:30:16 +0300

    pin ignition file to 2.4 so that it uses 3.4.0 ignition version
    Co-Authored-By: Nicklesh Adlakha <nicklesh.adlakha@gmail.com>

--- v2-okd-cluster-gandi-dns/workers/versions.tf
+++ v2-okd-cluster-gandi-dns/workers/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     ignition = {
       source  = "community-terraform-providers/ignition"
-      version = "~> 2.1"
+      version = "= 2.4"
     }
     openstack = {
       source = "terraform-provider-openstack/openstack"
